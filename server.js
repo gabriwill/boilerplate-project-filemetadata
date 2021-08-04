d1f87676e4ca5433cdfaf105c56dfa80161ee949
Gabriel Willans <69972816+gabriwill@users.noreply.github.com>
2021-08-04 20:43:55 -0300

    My solution for the file metadata challenge

--- server.js
+++ server.js
@@ -10,10 +10,19 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+//-----------------------------------------------------------
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
-
-
+//---------------------------------------------------------
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
